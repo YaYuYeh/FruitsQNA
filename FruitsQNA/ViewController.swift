ee585d49c9bf3ba14c59b93c949eaea0ab2a1bc9
YEHair <orange6718@gmail.com>
2022-08-21 21:53:21 +0800

    Initial Commit

---
+++ FruitsQNA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruitsQNA
+//
+//  Created by Ya Yu Yeh on 2022/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
